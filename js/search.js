a2b86aae137e709c90a59ce653a8550bf9267cd6
CHO <bsuysheep927@gmail.com>
2025-03-18 11:03:59 +0800

    search.html 新增 search.js 、調整 swiper屬性

---
+++ js/search.js
@@ -0,0 +1,69 @@
+$(function() {	
+	// 月刊有書書
+	let bookSwiper = new Swiper('.featured-book-swiper', {
+		// autoplay: true,
+    	loop: true,
+		slidesPerView: 5,
+		slidesPerGroup: 5,
+		spaceBetween: 30,
+		pagination: {
+			el: ".swiper-pagination",
+			clickable: true,
+		},
+		navigation: {
+			nextEl: ".swiper-button-next",
+			prevEl: ".swiper-button-prev",
+		},
+	});
+
+	// 月刊學習影音
+	let videoSwiper = new Swiper('.featured-video-swiper', {
+		autoplay: true,
+		loop: true,
+		slidesPerView: 4,
+		slidesPerGroup: 4,
+		spaceBetween: 20,
+		pagination: {
+			el: ".swiper-pagination",
+			clickable: true,
+		},
+		navigation: {
+			nextEl: ".swiper-button-next",
+			prevEl: ".swiper-button-prev",
+		},
+	});
+
+	// 巧虎影音
+	let tigerSwiper = new Swiper('.tiger-swiper', {
+		autoplay: true,
+		loop: true,
+		slidesPerView: 4,
+		slidesPerGroup: 4,
+		spaceBetween: 20,
+		pagination: {
+			el: ".swiper-pagination",
+			clickable: true,
+		},
+		navigation: {
+			nextEl: ".swiper-button-next",
+			prevEl: ".swiper-button-prev",
+		},
+	});
+
+	// 奇酷網影音
+	let kidSchoolSwiper = new Swiper('.kid-school-swiper', {
+		autoplay: true,
+		slidesPerView: 5,
+		slidesPerGroup: 5,
+		spaceBetween: 30,
+		loop: true,
+		pagination: {
+			el: ".swiper-pagination",
+			clickable: true,
+		},
+		navigation: {
+			nextEl: ".swiper-button-next",
+			prevEl: ".swiper-button-prev",
+		},
+	});
+});
