6a1a06201430a1c9516c51504e93ddc17ab09bfc
CHO <bsuysheep927@gmail.com>
2025-04-01 15:52:09 +0800

    巧連智：分類區塊，手機版時， 1.首頁不需要 acitve 項目，所以會導致順序異常，改成有 active 時才執行排序效果。 2.點擊時就會換頁了，所以拿掉點擊項目時更新效果。

--- js/public.js
+++ js/public.js
@@ -2,16 +2,17 @@ $(function() {
 	if($(window).width() <= 992) {
 		const $cols = $('.category-section .category-col');
 		let $activeCol = $cols.filter('.active');
-		
+
 		function updateOrder() {
 			// active 項目永遠是 order: 3
 			$activeCol.css('order', 3);
 			
 			const totalItems = $cols.length;
 			$cols.not('.active').each(function() {
+				
 				let distance = $(this).index() - $activeCol.index();
 				let newOrder;
-				
+
 				if(distance < 0) {
 					// 前面的項目
 					newOrder = 3 + distance;
@@ -27,15 +28,17 @@ $(function() {
 		}
 
 		// 點擊項目時更新順序
-		$cols.on('click', function() {
-			$cols.removeClass('active');
-			$(this).addClass('active');
-			$activeCol = $(this);
-			updateOrder();
-		});
+		// $cols.on('click', function() {
+		// 	$cols.removeClass('active');
+		// 	$(this).addClass('active');
+		// 	$activeCol = $(this);
+		// 	updateOrder();
+		// });
 
 		// 初始化順序
-		updateOrder();
+		if($activeCol.length !== 0) {
+			updateOrder();
+		}
 	}
 
 	// 密碼眼睛
