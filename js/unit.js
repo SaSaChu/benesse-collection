45773ddda41d80689ed81d061623061fb1f279a2
CHO <bsuysheep927@gmail.com>
2025-04-18 17:06:17 +0800

    巧連智前台：跳整手機版選單問題

--- js/unit.js
+++ js/unit.js
@@ -1,45 +1,59 @@
 $(function() {
 
-	let ageSwiper = new Swiper('.age-swiper', {
-		slidesPerView: 3,
-		spaceBetween: 16,
-		loop: true,	
-		navigation: {
-			nextEl: '.swiper-button-next',
-			prevEl: '.swiper-button-prev',
-		},
-		breakpoints: {
-			992: {
-				slidesPerView: 'auto',
-				spaceBetween: 30,
-				direction: 'horizontal',
-				loop: false,
+	if($('.age-swiper').length > 0) {
+		let activeIndex = $('.age-swiper .swiper-slide:has(.age-item.active)').index();
+		
+		let ageSwiper = new Swiper('.age-swiper', {
+				initialSlide: activeIndex,
+				centeredSlides: true,
+				slidesPerView: 3,
+				spaceBetween: 16,
+				loop: true,	
 				navigation: {
-					enabled: false
-				}
-				
-			}
-		},
-	});
-
-	if($('.sidebar-swiper').length > 0) {
-		let sidebarSwiper = new Swiper('.sidebar-swiper', {
-			direction: 'horizontal',
-			slidesPerView: 2.2,	
-			spaceBetween: 10,
-			loop: true,
+					nextEl: '.swiper-button-next',
+					prevEl: '.swiper-button-prev',
+			},
 			breakpoints: {
-				768: {
-					slidesPerView: 3,
-					spaceBetween: 15,
-				},
-				1024: {
-					direction: 'vertical',
+				992: {
+					centeredSlides: false,
 					slidesPerView: 'auto',
+					spaceBetween: 30,
+					direction: 'horizontal',
 					loop: false,
+					navigation: {
+						enabled: false
+					},
 				}
 			},
 		});
+	}
+
+	if($('.sidebar-swiper').length > 0) {
+		// 若沒有 select-wrapper 則進行初始化，有 select-wrapper 則直接在該頁面ajax時處理
+		if(!$('.select-wrapper').length) {
+			let activeIndex = $('.sidebar-swiper .swiper-slide:has(.sidebar-item.active)').index();
+			let slideCount = $('.sidebar-swiper .swiper-slide').length;
+			let sidebarSwiper = new Swiper('.sidebar-swiper', {
+				initialSlide: activeIndex,
+				centeredSlides: slideCount > 2.2 ? true : false,
+				direction: 'horizontal',
+				slidesPerView: 2.2,	
+				spaceBetween: 10,
+				loop: true,
+				breakpoints: {
+					768: {
+						slidesPerView: 3,
+						spaceBetween: 15,
+					},
+					992: {
+						centeredSlides: false,
+						direction: 'vertical',
+						slidesPerView: 'auto',
+						loop: false,
+					}
+				},
+			});
+		}
 	}
 
 	if($('.sidebar04-swiper').length > 0) {
@@ -71,7 +85,11 @@ $(function() {
 			$('.select-wrapper .dropdown-toggle').text(targetTitle);
 		});
 
+		let activeIndex = $('.sidebar04-swiper .swiper-slide:has(.sidebar-item.active)').index();
+		let slideCount = $('.sidebar04-swiper .swiper-slide').length;
 		let sidebar04Swiper = new Swiper('.sidebar04-swiper', {
+			initialSlide: activeIndex,
+			centeredSlides: slideCount > 2.2 ? true : false,
 			direction: 'horizontal',
 			slidesPerView: 2.2,	
 			spaceBetween: 10,
