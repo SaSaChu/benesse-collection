c382ec9e331c9502c480bfad9015cc479e458750
CHO <bsuysheep927@gmail.com>
2025-04-25 19:50:12 +0800

    巧連智前台：搜尋頁面，調整月刊有聲書網頁版和手機版選擇單元、調整輪播區塊

--- js/search.js
+++ js/search.js
@@ -1,69 +1,117 @@
 $(function() {	
 	// 月刊有書書
-	let bookSwiper = new Swiper('.featured-book-swiper', {
-		// autoplay: true,
-    	loop: true,
-		slidesPerView: 5,
-		slidesPerGroup: 5,
-		spaceBetween: 28,
-		pagination: {
-			el: ".swiper-pagination",
-			clickable: true,
-		},
-		navigation: {
-			nextEl: ".swiper-button-next",
-			prevEl: ".swiper-button-prev",
-		},
-	});
+	if ($('.featured-book-swiper').length > 0) {
+		let bookSwiper = new Swiper('.featured-book-swiper', {
+			// autoplay: true,
+	    	loop: true,
+			slidesPerView: 2,
+			slidesPerGroup: 2,
+			spaceBetween: 35,
+			breakpoints: {
+				768: {
+					slidesPerView: 3,
+					slidesPerGroup: 3,
+				},
+				1200: {
+					slidesPerView: 4,
+					slidesPerGroup: 4,
+				},
+			},
+			pagination: {
+				el: ".swiper-pagination",
+				clickable: true,
+			},
+			navigation: {
+				nextEl: ".swiper-button-next",
+				prevEl: ".swiper-button-prev",
+			},
+		});
+	}
 
 	// 月刊學習影音
-	let videoSwiper = new Swiper('.featured-video-swiper', {
-		// autoplay: true,
-		loop: true,
-		slidesPerView: 4,
-		slidesPerGroup: 4,
-		spaceBetween: 20,
-		pagination: {
-			el: ".swiper-pagination",
-			clickable: true,
-		},
-		navigation: {
-			nextEl: ".swiper-button-next",
-			prevEl: ".swiper-button-prev",
-		},
-	});
+	if ($('.featured-video-swiper').length > 0) {
+		let videoSwiper = new Swiper('.featured-video-swiper', {
+			// autoplay: true,
+			loop: true,
+			slidesPerView: 2,
+			slidesPerGroup: 2,
+			spaceBetween: 35,
+			breakpoints: {
+				768: {
+					slidesPerView: 3,
+					slidesPerGroup: 3,
+				},
+				1200: {
+					slidesPerView: 4,
+					slidesPerGroup: 4,
+				},
+			},
+			pagination: {
+				el: ".swiper-pagination",
+				clickable: true,
+			},
+			navigation: {
+				nextEl: ".swiper-button-next",
+				prevEl: ".swiper-button-prev",
+			},
+		});
+	}
 
 	// 巧虎影音
-	let tigerSwiper = new Swiper('.tiger-swiper', {
-		// autoplay: true,
-		loop: true,
-		slidesPerView: 4,
-		slidesPerGroup: 4,
-		spaceBetween: 20,
-		pagination: {
-			el: ".swiper-pagination",
-			clickable: true,
-		},
-		navigation: {
-			nextEl: ".swiper-button-next",
-			prevEl: ".swiper-button-prev",
-		},
-	});
+	if ($('.tiger-swiper').length > 0) {
+		let tigerSwiper = new Swiper('.tiger-swiper', {
+			// autoplay: true,
+			loop: true,
+			slidesPerView: 2,
+			slidesPerGroup: 2,
+			spaceBetween: 35,
+			breakpoints: {
+				768: {
+					slidesPerView: 3,
+					slidesPerGroup: 3,
+				},
+				1200: {
+					slidesPerView: 4,
+					slidesPerGroup: 4,
+				},
+			},
+			pagination: {
+				el: ".swiper-pagination",
+				clickable: true,
+			},
+			navigation: {
+				nextEl: ".swiper-button-next",
+				prevEl: ".swiper-button-prev",
+			},
+		});
+	}
 
 	// 奇酷網影音
-	let kidSchoolSwiper = new Swiper('.kid-school-swiper', {
-		// autoplay: true,
-		slidesPerView: 5,
-		slidesPerGroup: 5,
-		spaceBetween: 30,
-		loop: true,
-		pagination: {
-			el: ".swiper-pagination",
-			clickable: true,
-		},
-		navigation: {
-			nextEl: ".swiper-button-next",
-			prevEl: ".swiper-button-prev",
-		},
-	});
+	if ($('.kid-school-swiper').length > 0) {
+		let kidSchoolSwiper = new Swiper('.kid-school-swiper', {
+			// autoplay: true,
+			loop: true,
+			slidesPerView: 2,
+			slidesPerGroup: 2,
+			spaceBetween: 35,
+			breakpoints: {
+				768: {
+					slidesPerView: 3,
+					slidesPerGroup: 3,
+				},
+				1200: {
+					slidesPerView: 4,
+					slidesPerGroup: 4,
+				},
+			},
+			pagination: {
+				el: ".swiper-pagination",
+				clickable: true,
+			},
+			navigation: {
+				nextEl: ".swiper-button-next",
+				prevEl: ".swiper-button-prev",
+			},
+		});
+	}
 });
