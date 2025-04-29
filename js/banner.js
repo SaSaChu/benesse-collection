a01b6010aee2d39770b0e79a1b9e9e56f76efd7c
CHO <bsuysheep927@gmail.com>
2025-04-29 16:46:38 +0800

    巧連智前台：首頁新增公告 modal 及控制器，banner.js 加上防呆

--- js/banner.js
+++ js/banner.js
@@ -1,43 +1,47 @@
 $(function() {
-	let ageSwiper = new Swiper('.age-swiper', {
-		slidesPerView: 3,
-		spaceBetween: 16,
-		loop: true,	
-		navigation: {
-			nextEl: '.swiper-button-next',
-			prevEl: '.swiper-button-prev',
-		},
-		breakpoints: {
-			1024: {
-				slidesPerView: 'auto',
-				spaceBetween: 12,
-				direction: 'horizontal',
-				navigation: {
-					enabled: false
+	if($('.age-swiper').length > 0) {
+		let ageSwiper = new Swiper('.age-swiper', {
+			slidesPerView: 3,
+			spaceBetween: 16,
+			loop: true,	
+			navigation: {
+				nextEl: '.swiper-button-next',
+				prevEl: '.swiper-button-prev',
+			},
+			breakpoints: {
+				1024: {
+					slidesPerView: 'auto',
+					spaceBetween: 12,
+					direction: 'horizontal',
+					navigation: {
+						enabled: false
+					}
+					
 				}
-				
-			}
-		},
+			},
+		});
+	}
 
-	});
+	if($('.sidebar-swiper').length > 0) {
+		let sidebarSwiper = new Swiper('.sidebar-swiper', {
+			direction: 'horizontal',
+			slidesPerView: 3,
+			spaceBetween: 30,
+			loop: true,
+			breakpoints: {
+				1024: {
+					direction: 'vertical',
+					slidesPerView: 'auto',
+				}
+			},
+		});
+	}
 
-	let sidebarSwiper = new Swiper('.sidebar-swiper', {
-		direction: 'horizontal',
-		slidesPerView: 3,
-		spaceBetween: 30,
-		loop: true,
-		breakpoints: {
-			1024: {
-				direction: 'vertical',
-				slidesPerView: 'auto',
-			}
-		},
-	});
-
-	let bookSwiper = new Swiper('.featured-book-swiper', {
-		autoplay: true,
-    loop: true,
-    
+	if($('.featured-book-swiper').length > 0) {
+		let bookSwiper = new Swiper('.featured-book-swiper', {
+			autoplay: true,
+			loop: true,
+			
 		pagination: {
 			el: ".swiper-pagination",
 			clickable: true,
@@ -45,32 +49,42 @@ $(function() {
 		navigation: {
 			nextEl: ".swiper-button-next",
 			prevEl: ".swiper-button-prev",
-		},
-	});
+			},
+		});
+	}
 
-	let videoSwiper = new Swiper('.featured-video-swiper', {
-		autoplay: true,
-		loop: true,
-		
-		pagination: {
+	if($('.featured-video-swiper').length > 0) {
+		let videoSwiper = new Swiper('.featured-video-swiper', {
+			autoplay: true,
+			loop: true,
+			
+			pagination: {
 			el: ".swiper-pagination",
 			clickable: true,
 		},
 		navigation: {
 			nextEl: ".swiper-button-next",
 			prevEl: ".swiper-button-prev",
-		},
-	});
+			},
+		});
+	}
 
-	let carouselSwiper = new Swiper('.carousel-swiper', {
-		autoplay: true,
-		slidesPerView: '3',
-		spaceBetween: 20,
-		loop: true,
+	if($('.carousel-swiper').length > 0) {
+		let carouselSwiper = new Swiper('.carousel-swiper', {
+			autoplay: true,
+			slidesPerView: '3',
+			spaceBetween: 20,
+			loop: true,
 		breakpoints: {
 			1024: {
 				slidesPerView: 4,
 			},
-		},
-	});
+			},
+		});
+	}
+
+	if($('#noticebox').length > 0) {
+		let noticeBox = $('#noticebox');
+		noticeBox.modal('show');
+	}
 });
